3a284d5223f83b2cc4ba86e9f68a52051eb50d33
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-06 19:54:52 +0300

    Changes .gitignore App.css App.test.tsx App.tsx favicon.ico GlobalStyles.ts index.css index.html index.tsx logo.svg logo192.png logo512.png manifest.json package.json react-app-env.d.ts README.md reportWebVitals.ts robots.txt setupTests.ts Theme.Styled.ts Todolist.tsx tsconfig.json WhatPurposesObjectCanBeUsedFor.tsx yarn.lock

---
+++ src/Todolist.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { v1 } from 'uuid';
+import {FilterValuesType} from './App';
+
+type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: string) => void
+    changeFilter: (value: FilterValuesType) => void
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input type="button" name="" value=""/>
+            <button>+</button>
+        </div>
+        <ul>
+            {
+                props.tasks.map(t => {
+                    const uuid = v1();
+                    return(<li key={t.id}>
+                    <input type="checkbox" id={`checkbox${uuid}`} defaultChecked={t.isDone} />
+                    <label htmlFor={`checkbox${uuid}`}>{t.title}</label>
+                    <button onClick={() => {
+                        props.removeTask(t.id)
+                    }}>x
+                    </button>
+                </li>)})
+            }
+        </ul>
+        <div>
+            <button onClick={() => {
+                props.changeFilter("all")
+            }}>
+                All
+            </button>
+            <button onClick={() => {
+                props.changeFilter("active")
+            }}>
+                Active
+            </button>
+            <button onClick={() => {
+                props.changeFilter("completed")
+            }}>
+                Completed
+            </button>
+        </div>
+    </div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {ThemeProvider} from "styled-components";
+import theme from "./styles/Theme.Styled";
+import {GlobalStyles} from "./styles/GlobalStyles";
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <ThemeProvider theme={theme}>
+        <GlobalStyles/>
+        <App/>
+        {/*<WhatPurposesObjectCanBeUsedFor/>*/}
+    </ThemeProvider>
+);
